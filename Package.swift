9abde38fbcdd7d99c2038fc21c7946c80ec4bf6d
Build Er <builder@macmini6.local>
2023-02-16 23:51:19 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXDebugger",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXDebugger",
+			targets: ["GXDebuggerWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXDebuggerWrapper",
+				dependencies: [
+					"GXDebugger",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXDebugger",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDebugger-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "7a489c2b923a7f9e990929d718d31b64e17f250ccb197d95491fa374d5dacf47"
+		)
+	]
+)
