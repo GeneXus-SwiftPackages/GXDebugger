14cf1d7aa67746f54f689ca90929ef097c3b202e
genexusbot <builder@genexus.com>
2023-04-11 14:48:15 -0300

    Package version: 1.0.0-beta+20230411173940

--- Package.swift
+++ Package.swift
@@ -19,14 +19,14 @@ let package = Package(
 				dependencies: [
 					"GXDebugger",
 					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .tvOS, .iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
 					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDebugger",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDebugger-1.0.0-beta+20230405133406.xcframework.zip",
-			checksum: "db0701ee5baf1da9054da6d3de05d4457d1906e68889ad6910940d0a864c73c9"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDebugger-1.0.0-beta+20230411173940.xcframework.zip",
+			checksum: "6f1ff0a246f14c34c15999cfb17f980245a3135bde9754434224f54066b926fe"
 		)
 	]
 )
