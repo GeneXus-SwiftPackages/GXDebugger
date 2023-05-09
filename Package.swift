2b418b722d34754f37c15fd0ee80e3bfcc6a0b94
Build Er <builder@macmini6.local>
2023-05-09 14:41:09 -0300

    Package version: 1.0.0-beta+20230509173316

--- Package.swift
+++ Package.swift
@@ -25,8 +25,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDebugger",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDebugger-1.0.0-beta+20230509131236.xcframework.zip",
-			checksum: "cd1245718f73c69fac074d370f4840d9913bd29338fde47cd8a6a4e1955f8b23"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDebugger-1.0.0-beta+20230509173316.xcframework.zip",
+			checksum: "b0e27f06cacfabdb0f8fa49f7e859af3f2c8145e3a5ea4ea959d0d504bcaa5bb"
 		)
 	]
 )
